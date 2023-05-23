3bbe60f8bc29032b1fffec03f4f982cd1b5553e4
diegosantarelli <s1099386@studenti.univpm.it>
2023-05-23 13:03:04 +0200

    cambiata rotta modifica faq in gestione faq

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -6,7 +6,7 @@
 @can('isAdmin')
 <div class="gestione-faq">
   <a href="{{ route('insertfaq') }}">Inserisci Faq</a>
-  <a href="{{ route('modificafaq') }}">Modifica Faq</a>
+  <a href="{{ route('gestionefaq') }}">Modifica Faq</a>
   <a href="{{ route('deletefaq') }}">Elimina Faq</a>
   
 </div>
