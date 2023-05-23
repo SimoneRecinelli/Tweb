12b8ae3f75d822a0ada0a624589014e05e1d586d
diegosantarelli <s1099386@studenti.univpm.it>
2023-05-23 17:20:48 +0200

    homestaff

--- resources/views/homestaff.blade.php
+++ resources/views/homestaff.blade.php
@@ -5,5 +5,14 @@
         <h3>Area Membro dello Staff</h3>
         <p>Benvenuto {{ Auth::user()->nome }} {{ Auth::user()->cognome }}</p>
         <p>Seleziona la funzione da attivare</p>
-    </div>
+
+        <h2> Gestione offerte</h2>
+<a href="{{route('insertofferta')}}">inserisci un'offerta'</a>
+<a href="{{route('deleteofferta')}}">elimina un'offerta'</a>
+<a href="{{route('modificaofferta')}}">modifica un'offerta'</a>
+
+</div>
+
+
+    
 @endsection
