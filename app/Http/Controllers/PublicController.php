e7f95cb9a60c29ff3723dfe10f4b2d4fa952a233
DavideTicchiarelli <s1098361@studenti.univpm.it>
2023-05-23 17:31:20 +0200

    aggiunto paginate faq e limitato offerte evid home a  4

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -17,100 +17,34 @@ class PublicController extends Controller
 {
 
     protected $_catalogModel;
-    protected $_companyModel;
-
-
+    
     public function __construct() {
         $this->_catalogModel = new Catalog;
-        $this->_companyModel = new Azienda;
-
-
     }
 
 
     public function showHome(): View
-    {
-        $offerteInEvidenza = Offerta::where('Evidenza', 'sì')->get();
-        return view('home', ['offerte' => $offerteInEvidenza]);
-    }
+{
+    $offerteInEvidenza = Offerta::getOfferteEvidenza();
+    return view('home', ['offerte' => $offerteInEvidenza]);
+}
     
-
-/*
-    
-    public function showCatalog1() {
-
-        //Categorie Top
-        $topCats = $this->_catalogModel->getTopCats();
-
-        //Prodotti in sconto di tutte le categorie, ordinati per sconto decrescente
-        $prods = $this->_catalogModel->getProdsByCat($topCats->map->only(['catId']), 2, 'desc', true);
-
-        return view('catalog')
-                        ->with('topCategories', $topCats)
-                        ->with('products', $prods);
-    }
-
-    public function showCatalog2($topCatId) {
-
-        //Categorie Top
-        $topCats = $this->_catalogModel->getTopCats();
-
-        //Categoria Top selezionata
-        $selTopCat = $topCats->where('catId', $topCatId)->first();
-
-        // Sottocategorie
-        $subCats = $this->_catalogModel->getCatsByParId([$topCatId]);
-
-        //Prodotti in sconto della categoria Top selezionata, ordinati per sconto decrescente
-        $prods = $this->_catalogModel->getProdsByCat([$topCatId], 2, 'desc', true);
-
-        return view('catalog')
-                        ->with('topCategories', $topCats)
-                        ->with('selectedTopCat', $selTopCat)
-                        ->with('subCategories', $subCats)
-                        ->with('products', $prods);
-    }
-
-    public function showCatalog3($topCatId, $catId) {
-
-        //Categorie Top
-        $topCats = $this->_catalogModel->getTopCats();
-
-        //Categoria Top selezionata
-        $selTopCat = $topCats->where('catId', $topCatId)->first();
-
-        // Sottocategorie
-        $subCats = $this->_catalogModel->getCatsByParId([$topCatId]);
-
-        // Prodotti della categoria selezionata, in sconto o meno
-       $prods = $this->_catalogModel->getProdsByCat([$catId]);
-
-        return view('catalog')
-                        ->with('topCategories', $topCats)
-                        ->with('selectedTopCat', $selTopCat)
-                        ->with('subCategories', $subCats)
-                        ->with('products', $prods);
-    }
-
-*/
 
 public function showCatalog($Categoria='Animali'): View {
     $offerte = $this->_catalogModel->getOffByCat($Categoria);
     return view('catalog')->with('offerte',$offerte);
-       
 } 
 
-public function showAziende(){ 
-    $aziende=$this->_companyModel->getAziende();
-    $aziende=Azienda::paginate(1);
-    return view('aziende')
-         ->with('aziende',$aziende);
+public function showAziende() { 
+    $aziende = Azienda::paginate(4);
+    return view('aziende', compact('aziende'));
 }
 
-/*public function showAziende()
+
+/* funzione prima di inserire il paginate
+public function showAziende()
 {
     $aziende = Azienda::all();
-    
     return view('aziende', compact('aziende'));
 }*/
 
@@ -124,10 +58,18 @@ public function showSingleAzienda($id): View
 /**
      * Show faq page for a public user.
      */
-    public function showFaq(): View {
+
+
+     public function showFaq(): View {
+        $faqs = Faq::paginate(4);
+        return view('faq', compact('faqs'));
+    }
+    
+
+  /*  public function showFaq(): View {
         $faqs=Faq::all();
         return view('faq')->with('faqs',$faqs);
-    }
+    }*/
     /**
      * Show info page for a public user.
      */
