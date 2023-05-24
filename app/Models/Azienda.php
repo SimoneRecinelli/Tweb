dab379c13237e7acab367863669be636d93ea7f4
LoriGaetani <100944852+LoriGaetani@users.noreply.github.com>
2023-05-24 11:52:07 +0200

    select azienda

--- app/Models/Azienda.php
+++ app/Models/Azienda.php
@@ -10,6 +10,22 @@ class Azienda extends Model
     public $timestamps = false;
 
     public function getAziende(){
-        return Azienda::all();
+
+        return Azienda::all()->pluck('NomeAzienda');
     }
+
+    //va messa nella select di iserimento offerta per estrarre il nome 
+    public function getNome($num){
+        $i=0;
+        $aziende=Azienda::all();
+        foreach($aziende as $azienda){
+            if($i==$num){
+                return $azienda->NomeAzienda;break;
+        }
+        else{
+            $i++;
+        }
+        }
+    }
+
 }
