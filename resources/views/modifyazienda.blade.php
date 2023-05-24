69e8adfd8071afc732f9552c503d032ab38336d1
DavideTicchiarelli <s1098361@studenti.univpm.it>
2023-05-24 14:30:32 +0200

    ho messo un pò di jquery

--- resources/views/modifyazienda.blade.php
+++ resources/views/modifyazienda.blade.php
@@ -12,7 +12,7 @@
 @method('PUT')
 {{ Form::token() }}
 {{ Form::label('NomeAzienda', 'Nome Azienda') }}
-    {{ Form::textArea('NomeAzienda', $azienda->NomeAzienda, ['class' => 'form-control']) }}
+    {{ Form::textArea('NomeAzienda', $azienda->NomeAzienda, ['class' => 'form-control', 'id' => 'expandingTextarea']) }}
     @if ($errors->first('NomeAzienda'))
                 <ul class="errors">
                     @foreach ($errors->get('NomeAzienda') as $message)
@@ -22,7 +22,7 @@
                 @endif
 
     {{Form::label('Sede', 'Sede') }}
-    {{ Form::textArea('Sede', $azienda->Sede, ['class' => 'form-control']) }}
+    {{ Form::textArea('Sede', $azienda->Sede, ['class' => 'form-control', 'id' => 'expandingTextarea']) }}
     @if ($errors->first('Sede'))
                 <ul class="errors">
                     @foreach ($errors->get('Sede') as $message)
@@ -32,7 +32,7 @@
                 @endif
 
     {{Form::label('Tipologia', 'Tipologia') }}
-    {{ Form::textArea('Tipologia', $azienda->Tipologia, ['class' => 'form-control']) }}
+    {{ Form::textArea('Tipologia', $azienda->Tipologia, ['class' => 'form-control', 'id' => 'expandingTextarea']) }}
     @if ($errors->first('Tipologia'))
                 <ul class="errors">
                     @foreach ($errors->get('Tipologia') as $message)
@@ -42,7 +42,7 @@
                 @endif
 
     {{Form::label('RagioneSociale', 'RagioneSociale') }}
-    {{ Form::textArea('RagioneSociale', $azienda->RagioneSociale, ['class' => 'form-control']) }}
+    {{ Form::textArea('RagioneSociale', $azienda->RagioneSociale, ['class' => 'form-control', 'id' => 'expandingTextarea']) }}
     @if ($errors->first('RagioneSociale'))
                 <ul class="errors">
                     @foreach ($errors->get('RagioneSociale') as $message)
@@ -57,4 +57,27 @@
 
             </div>
 </section>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+<script>
+$(document).ready(function() {
+  // Seleziona tutte le textarea con l'ID "expandingTextarea" e aggiungi un gestore per l'evento di input
+  $('#expandingTextarea').on('input', function() {
+    // Imposta l'altezza minima della textarea (puoi modificarla a tuo piacimento)
+    var minHeight = 100;
+    // Imposta l'altezza massima della textarea (puoi modificarla a tuo piacimento)
+    var maxHeight = 300;
+    
+    // Calcola l'altezza necessaria in base al contenuto
+    var scrollHeight = this.scrollHeight;
+    // Limita l'altezza tra il valore minimo e massimo
+    var height = Math.min(maxHeight, Math.max(scrollHeight, minHeight));
+    
+    // Imposta la nuova altezza della textarea
+    $(this).css('height', height + 'px');
+  });
+});
+</script>
+
 @endsection
