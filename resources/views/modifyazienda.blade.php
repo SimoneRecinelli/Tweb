8f7866ff880c822286509a61f8f4d91c52dfdb1f
DavideTicchiarelli <s1098361@studenti.univpm.it>
2023-05-24 14:09:16 +0200

    fix css form crud

--- resources/views/modifyazienda.blade.php
+++ resources/views/modifyazienda.blade.php
@@ -1,10 +1,18 @@
 @extends('public')
 @section('content')
-{{ Form::open(array('route' => ['modifyazienda', $azienda->idAzienda], 'method' => 'POST')) }}
+
+<link rel="stylesheet" type="text/css" href="{{asset('css/formCRUD.css')}}">
+
+<section class="form_section">
+<div class="card">
+<h2 class="titolo"> Modifica Azienda</h2>
+
+
+{{ Form::open(array('route' => ['modifyazienda', $azienda->idAzienda], 'method' => 'POST', 'class' => 'form-wrapper')) }}
 @method('PUT')
 {{ Form::token() }}
-{{ Form::label('NomeAzienda', 'NomeAzienda') }}
-    {{ Form::text('NomeAzienda', $azienda->NomeAzienda, ['class' => 'form-control']) }}<br>
+{{ Form::label('NomeAzienda', 'Nome Azienda') }}
+    {{ Form::text('NomeAzienda', $azienda->NomeAzienda, ['class' => 'form-control']) }}
     @if ($errors->first('NomeAzienda'))
                 <ul class="errors">
                     @foreach ($errors->get('NomeAzienda') as $message)
@@ -14,7 +22,7 @@
                 @endif
 
     {{Form::label('Sede', 'Sede') }}
-    {{ Form::text('Sede', $azienda->Sede, ['class' => 'form-control']) }}<br>
+    {{ Form::text('Sede', $azienda->Sede, ['class' => 'form-control']) }}
     @if ($errors->first('Sede'))
                 <ul class="errors">
                     @foreach ($errors->get('Sede') as $message)
@@ -24,7 +32,7 @@
                 @endif
 
     {{Form::label('Tipologia', 'Tipologia') }}
-    {{ Form::text('Tipologia', $azienda->Tipologia, ['class' => 'form-control']) }}<br>
+    {{ Form::text('Tipologia', $azienda->Tipologia, ['class' => 'form-control']) }}
     @if ($errors->first('Tipologia'))
                 <ul class="errors">
                     @foreach ($errors->get('Tipologia') as $message)
@@ -34,7 +42,7 @@
                 @endif
 
     {{Form::label('RagioneSociale', 'RagioneSociale') }}
-    {{ Form::text('RagioneSociale', $azienda->RagioneSociale, ['class' => 'form-control']) }}<br>
+    {{ Form::text('RagioneSociale', $azienda->RagioneSociale, ['class' => 'form-control']) }}
     @if ($errors->first('RagioneSociale'))
                 <ul class="errors">
                     @foreach ($errors->get('RagioneSociale') as $message)
@@ -43,6 +51,10 @@
                 </ul>
                 @endif
             </a>
-            {{ Form::submit('Modifica azienda', ['class' => 'btn btn-primary']) }}
+            {{ Form::submit('Modifica azienda', ['class' => 'btn-modify']) }}
             {{ Form::close() }}  
+
+
+            </div>
+</section>
 @endsection
