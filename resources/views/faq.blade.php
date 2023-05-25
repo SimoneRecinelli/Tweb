6faed8d989aa1128addd28afc9b374db5db8c16b
diegosantarelli <s1099386@studenti.univpm.it>
2023-05-25 17:41:31 +0200

    hr faq

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -4,6 +4,7 @@
 <link rel="stylesheet" type="text/css" href="{{asset('css/Faq.css')}}">
 <link rel='stylesheet' href='https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css'>
 
+<hr>
 
 @can('isAdmin')
 <div class="gestione-faq">
