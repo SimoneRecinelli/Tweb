fd277c33a9ade3f00dcbcabe565ca89a8974a3ad
DavideTicchiarelli <s1098361@studenti.univpm.it>
2023-05-29 12:24:38 +0200

    aggiunti placeholder insert faq

--- resources/views/AdminViews/insertfaq.blade.php
+++ resources/views/AdminViews/insertfaq.blade.php
@@ -1,39 +1,40 @@
 @extends('public')
 
 @section('content')
-<link rel="stylesheet" type="text/css" href="{{asset('css/formCRUD.css')}}">
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="{{ asset('js/TextArea.js') }}"></script>
-<section class="form_section">
-<div class="card">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/formCRUD.css')}}">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="{{ asset('js/TextArea.js') }}"></script>
+    <section class="form_section">
+        <div class="card">
+            <h2 class="titolo">INSERISCI FAQ</h2>
 
-        <h2 class="titolo">INSERISCI FAQ</h2>
-    
-{{ Form::open(array('route' => 'storefaq', 'class' => 'form-wrapper' )) }}
-    @csrf
-    {{ Form::token() }}
-{{ Form::label('Domanda', 'Domanda') }}
-    {{ Form::textArea('Domanda', null, ['class' => 'form-control']) }}
-    @if ($errors->first('Domanda'))
+            {{ Form::open(array('route' => 'storefaq', 'class' => 'form-wrapper')) }}
+            @csrf
+            {{ Form::token() }}
+            
+            {{ Form::label('Domanda', 'Domanda') }}
+            {{ Form::textArea('Domanda', null, ['class' => 'form-control', 'placeholder' => 'Inserisci la domanda']) }}
+            @if ($errors->first('Domanda'))
                 <ul class="errors">
                     @foreach ($errors->get('Domanda') as $message)
-                    <li>{{ $message }}</li>
+                        <li>{{ $message }}</li>
                     @endforeach
                 </ul>
-                @endif
+            @endif
 
-    {{Form::label('Risposta', 'Risposta') }}
-    {{ Form::textArea('Risposta', null, ['class' => 'form-control']) }}
-    @if ($errors->first('Risposta'))
+            {{ Form::label('Risposta', 'Risposta') }}
+            {{ Form::textArea('Risposta', null, ['class' => 'form-control', 'placeholder' => 'Inserisci la risposta']) }}
+            @if ($errors->first('Risposta'))
                 <ul class="errors">
                     @foreach ($errors->get('Risposta') as $message)
-                    <li>{{ $message }}</li>
+                        <li>{{ $message }}</li>
                     @endforeach
                 </ul>
-                @endif
-                {{ Form::submit('Crea faq', ['class' => 'btn-modify']) }}
-    {{ Form::close() }}
+            @endif
 
-</div>
-</section>
+            {{ Form::submit('Crea faq', ['class' => 'btn-modify']) }}
+            {{ Form::close() }}
+            
+        </div>
+    </section>
 @endsection
