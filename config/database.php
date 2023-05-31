48cdb3f763aac30360ed855bc7c7943158c52f04
SimoneRecinelli <simonerecinelli@gmail.com>
2023-05-31 13:08:33 +0200

    cambiato db per caricamento server

--- config/database.php
+++ config/database.php
@@ -40,11 +40,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $HOST,
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', ''),
-            'username' => env('DB_USERNAME', ''),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => $DB,
+            'username' => $USER,
+            'password' => $PASSWORD,
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -56,8 +56,8 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
-
     ],
+
 
     /*
     |--------------------------------------------------------------------------
